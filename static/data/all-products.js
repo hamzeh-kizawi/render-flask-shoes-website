115e29c098111f59ed679bea0062f7e75cc93a86
Hamza <hamzeh.kizawi11@e-uvt.ro>
2024-12-25 10:44:32 +0200

    add products to all-products.js

---
+++ static/data/all-products.js
@@ -0,0 +1,142 @@
+export const allProduct = [
+  {
+    productId: "1",
+    productImage: "/static/shoes-images/men-shoes1.png",
+    productPrice: "120",
+    productName: "Yellow Nike Blaze",
+    status: ""
+  },
+  {
+    productId: "2",
+    productImage: "/static/shoes-images/men-shoes2.png",
+    productPrice: "80",
+    productName: "Nike Monochrome Glide",
+    status: ""
+  },
+  {
+    productId: "3",
+    productImage: "/static/shoes-images/men-shoes3.png",
+    productPrice: "$ 95",
+    productName: "Nike Neon Sprint",
+    status: ""
+  },
+  {
+    productId: "4",
+    productImage: "/static/shoes-images/men-shoes4.png",
+    productPrice: "$ 100",
+    productName: "Nike Steel Streak",
+    status: ""
+  },
+  {
+    productId: "5",
+    productImage: "/static/shoes-images/men-shoes5.png",
+    productPrice: "$ 220",
+    productName: "Nike Orange Flash",
+    status: ""
+  },
+  {
+    productId: "6",
+    productImage: "/static/shoes-images/men-shoes6.png",
+    productPrice: "$ 110",
+    productName: "Retro Kick",
+    status: ""
+  },
+  {
+    productId: "7",
+    productImage: "/static/shoes-images/men-shoes7.png",
+    productPrice: "$ 180",
+    productName: "Nike Aqua Pulse",
+    status: ""
+  },
+  {
+    productId: "8",
+    productImage: "/static/shoes-images/men-shoes8.png",
+    productPrice: "$ 140",
+    productName: "Gray Ghost",
+    status: ""
+  },
+  {
+    productId: "9",
+    productImage: "/static/shoes-images/women-shoes1.png",
+    productPrice: "$ 100",
+    productName: "Urban Glacier",
+    status: ""
+  },
+  {
+    productId: "10",
+    productImage: "/static/shoes-images/women-shoes2.png",
+    productPrice: "$ 90",
+    productName: "Pink Surge",
+    status: ""
+  },
+  {
+    productId: "11",
+    productImage: "/static/shoes-images/women-shoes3.png",
+    productPrice: "$ 195",
+    productName: "Cherry Sprint",
+    status: ""
+  },
+  {
+    productId: "12",
+    productImage: "/static/shoes-images/women-shoes4.png",
+    productPrice: "$ 105",
+    productName: "Crimson Vortex",
+    status: ""
+  },
+  {
+    productId: "13",
+    productImage: "/static/shoes-images/women-shoes5.png",
+    productPrice: "$ 200",
+    productName: "Firebolt Runner",
+    status: ""
+  },
+  {
+    productId: "14",
+    productImage: "/static/shoes-images/women-shoes6.png",
+    productPrice: "$ 135",
+    productName: "Electric Wave",
+    status: ""
+  },
+  {
+    productId: "15",
+    productImage: "/static/shoes-images/women-shoes7.png",
+    productPrice: "$ 160",
+    productName: "Midnight Racer",
+    status: ""
+  },
+  {
+    productId: "16",
+    productImage: "/static/shoes-images/women-shoes8.png",
+    productPrice: "$ 110",
+    productName: "Scarlet Lockdown",
+    status: ""
+  },
+  {
+    productId: "17",
+    productImage: "/static/shoes-images/sales-shoes1.png",
+    productPrice: "$ 144",
+    productName: "Yellow Nike Blaze",
+    status: "- 20%"
+  },
+  {
+    productId: "18",
+    productImage: "/static/shoes-images/sales-shoes2.png",
+    productPrice: "$ 96",
+    productName: "Nike Monochrome Glide",
+    status: "- 20%"
+  },
+  {
+    productId: "19",
+    productImage: "/static/shoes-images/sales-shoes3.png",
+    productPrice: "$ 128",
+    productName: "Nike Neon Sprint",
+    status: "- 20%"
+  },
+  {
+    productId: "20",
+    productImage: "/static/shoes-images/sales-shoes4.png",
+    productPrice: "$ 144",
+    productName: "Nike Steel Streak",
+    status: "- 20%"
+  },
+]
