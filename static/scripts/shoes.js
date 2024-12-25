23118714fe52e0304b54dcee1324dbad220b74aa
Hamza <hamzeh.kizawi11@e-uvt.ro>
2024-12-25 19:56:01 +0200

    make the cart quantity interactive

--- static/scripts/shoes.js
+++ static/scripts/shoes.js
@@ -1,5 +1,7 @@
 import { allProduct} from "/static/data/all-products.js";
 
+/* shoes generation with js*/
+
 const menEl = document.querySelector(".men-box")
 
 allProduct.men.forEach((item)=>{
@@ -20,7 +22,8 @@ allProduct.men.forEach((item)=>{
         <div class="product-text">
           ${item.productName}
         </div>
-        <button class="add-btn">Add to cart</button>
+        <button class="add-btn add-btn-js"
+        data-product-id = "${item.productId}">Add to cart</button>
       </div>
   `;
 
@@ -46,7 +49,8 @@ allProduct.women.forEach((item)=>{
         <div class="product-text">
           ${item.productName}
         </div>
-        <button class="add-btn">Add to cart</button>
+        <button class="add-btn add-btn-js"
+        data-product-id = "${item.productId}">Add to cart</button>
       </div>
   `
 
@@ -76,12 +80,54 @@ allProduct.sales.forEach((item)=>{
           <div class="product-text">
             ${item.productName}
           </div>
-          <button class="add-btn">Add to cart</button>
+          <button class="add-btn add-btn-js"
+          data-product-id = "${item.productId}">Add to cart</button>
         </div>
   `
 
   salesEl.appendChild(salesDiv);
-})
+});
+
+/* end shoes generation with js*/
+
+
+/* add to cart button */
+ document.querySelectorAll(".add-btn-js")
+  .forEach((button)=>{
+    button.addEventListener('click', ()=>{
+      const productId = button.dataset.productId;
+
+      let matchingItem;
+
+      cart.forEach((item)=>{
+        if(productId === item.productId){
+          matchingItem = item;
+        }
+      });
+
+      if(matchingItem){
+        matchingItem.amount += 1
+      } else {
+        cart.push({
+          productId: productId,
+          amount: 1
+        });
+      }
+
+      let cartQuantity = 0;
+
+      cart.forEach((item)=>{
+        cartQuantity += item.amount;
+      });
+
+      document.querySelector(".cart-quantity-js").innerHTML = cartQuantity; 
+    });
+  });
+
+/* end add to cart button */
+
+
+
 
 
 
